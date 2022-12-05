b098325064ee71afa41e338b0f17376e067e0143
tasoulis <kaputtebriefen@gmail.com>
2022-12-05 17:00:47 +0200

    exercise2

---
+++ C/ex2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+   FILE *fp;
+   char c;
+   
+  
+   fp = fopen("C:\\Users\\tasoulis\\Projects\\Play\\C\\SortText.txt","r");
+   if(fp == NULL) 
+   {
+      perror("Error in opening file");
+      return(-1);
+   } 
+   do 
+   {
+      c = fgetc(fp);
+      if( feof(fp) ) 
+      {
+         break ;
+      }
+      printf("%c", c);
+   } while(1);
+
+   fclose(fp);
+   return(0);
+}
