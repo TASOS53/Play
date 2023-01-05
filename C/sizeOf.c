fd6c46f426c1630b1dc07a2f98911448e95871b6
tasoulis <kaputtebriefen@gmail.com>
2023-01-05 17:55:47 +0200

    The size of different types of integer

---
+++ C/sizeOf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+    
+    printf("The size of int is: %lld\n", sizeof(int));
+    printf("The size of long int is: %lld\n", sizeof(long int));
+    printf("The size of long long int is: %lld\n", sizeof(long long int));
+    
+    printf("The size of unsigned int is: %lld\n", sizeof(unsigned int));
+    printf("The size of unsigned long int is: %lld\n", sizeof(unsigned long int));
+    printf("The size of unsigned long long int is: %lld\n", sizeof(unsigned long long int));
+
+
+
+
+    return 0;
+}
